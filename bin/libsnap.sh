8a9c57e57b98e3873ae82ee2dd971e6367fe18e0
Scott Weikart <sweikart@gmail.com>
2019-02-27 22:45:22 -0800

    add copyright notice

--- bin/libsnap.sh
+++ bin/libsnap.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+#     libsnap.sh is a library used by snap aka snapshot, snapback, & snapcrypt
+#
+#     Copyright (C) 2018-2019, Human Rights Data Analysis Group (HRDAG)
+#     https://hrdag.org
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #############################################################################
 #############################################################################
 ### This file sets up a standard environment and defines basic functions.
