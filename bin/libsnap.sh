da99d18f313e37676e9c6282dec726919a32cf8a
Scott Weikart <sweikart@gmail.com>
2019-03-07 12:53:19 -0800

    need to initially call remember_tracing if not sure suspend_tracing will be called before restore_tracing

--- bin/libsnap.sh
+++ bin/libsnap.sh
@@ -571,6 +571,15 @@ TraceV() { is_num $1; (($1 <= Trace_level)) ||return 1;shift; echoEV -1 "$@"; }
 
 declare -A funcname2was_tracing		# global for next two functions
 
+function remember_tracing {
+
+	if [[ -o xtrace ]]
+	   then local was_tracing=$true
+	   else local was_tracing=$false
+	fi
+	funcname2was_tracing[ ${FUNCNAME[1]} ]=$was_tracing
+}
+# above and below functions are almost identical
 function suspend_tracing {
 
 	if [[ -o xtrace ]]
